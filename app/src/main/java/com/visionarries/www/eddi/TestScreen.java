a0d04ef9a95c51e7e8fa0bdbcd3c15cba5d4c90d
Crostiphex <crostiphex@gmail.com>
2015-02-09 14:25:23 -0500

    Progressbar

--- app/src/main/java/com/visionarries/www/eddi/TestScreen.java
+++ app/src/main/java/com/visionarries/www/eddi/TestScreen.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
